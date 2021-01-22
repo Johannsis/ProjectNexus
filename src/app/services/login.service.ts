542a1747f7513a5dfb0c8e675c5b27676316734d
Johannes <johanneshoersch@gmail.com>
2021-01-22 14:32:41 -0400

    Fourth Commit

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,11 +7,13 @@ import { HttpClient } from '@angular/common/http';
 export class LoginService {
 
   public user: string = null;
+  public username: string = null;
 
   constructor(public _http: HttpClient) {}
   
 
   public login(username: string, password: string){
+    this.username = username;
     return this._http.post('https://cors-anywhere.herokuapp.com/https://courierdemo.azurewebsites.net/api/membership/login', {
       username: username, 
       password: password
--- src/app/services/packages.service.ts
+++ src/app/services/packages.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { LoginService } from '../services/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PackagesService {
 
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient,
+              public loginService: LoginService) {}
+
   
-  readonly user: string = "jsanchez";
+  currentuser = this.loginService.username;
 
   getPackages(){
-    return this._http.get('https://cors-anywhere.herokuapp.com/https://courierdemo.azurewebsites.net/api/packages/getPending?username=' + this.user);
+    return this._http.get('https://cors-anywhere.herokuapp.com/https://courierdemo.azurewebsites.net/api/packages/getPending?username=' + this.currentuser);
   }
 
 }
